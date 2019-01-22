598ca3df25c880139b9d28b9f6a5b6222d73133d
alexgomel <alexgomel@tut.by>
2019-01-22 10:29:13 +0300

    fix for empty categ

--- app/UseCases/Categories/CategoryService.php
+++ app/UseCases/Categories/CategoryService.php
@@ -12,13 +12,13 @@ class CategoryService
     {
         //select root.name_categ,root.slug,count(news.id) as countnews from categs as root
         //inner join categs as innercateg on (innercateg._lft between root._lft and root._rgt)
-        //inner join news on (innercateg.id = news.categ_id)
+        //left join news on (innercateg.id = news.categ_id)
         //where root.parent_id is null
         //group by root.name_categ,root.slug
 
         return DB::table('categs as root')->select(DB::raw('root.name_categ, root.slug, count(news.id) as countnews'))
             ->joinWhere('categs as innercateg', DB::raw('true'),DB::raw('innercateg._lft between root._lft and root._rgt'), '')
-            ->join('news', 'innercateg.id', '=', 'news.categ_id')
+            ->leftJoin('news', 'innercateg.id', '=', 'news.categ_id')
             ->where(['root.parent_id'=>null])
             ->groupBy(['root.name_categ', 'root.slug'])
             ->get();
